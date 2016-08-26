47fbf1398fb9fbbe52bca90be817635d4d4ba82c
hal13 <vagrant@enpit>
2016-08-26 11:46:57 +0000

    finished 17

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,6 +25,15 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
   
+  def update
+    @project = Projects.find(params[:id])
+    if @project.update
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
     def project_params
