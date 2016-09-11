04d39e99cc83070e09e6238c11ad02fbb905a55d
hal13 <mitsu_yama007@ae.auone-net.jp>
2016-09-11 15:03:02 +0000

    add checkbox

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,32 @@
 class TasksController < ApplicationController
-  
+  before_action :find_project, only: [:create]
+
   def create
-    @project = Project.find(params[:project_id])
     @task = @project.tasks.create(task_params)
     redirect_to project_path(@project.id)
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to project_path(@project.id)
+  end
+  
+  def toggle
+    render nothing: true
+    @task = Task.find(params[:id])
+    @task.done = !@task.done
+    @task.save
   end
   
   private
   
   def task_params
     params[:task].permit(:title)
+  end
+  
+  def find_project
+    @project = Project.find(params[:project_id])
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Taskapp::Application.routes.draw do
   resources :projects do
     resources :tasks, only: [:create, :destroy]
   end
+  
+  post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
 
   root 'projects#index'
 
